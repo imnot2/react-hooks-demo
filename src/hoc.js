d5fe04904f9e7b705b85a8043aafe8cfe128433e
Bigger.Ding <bigui.ding@gmail.com>
2019-08-13 20:26:02 +0800

    adding HOC RenderProps component

---
+++ src/hoc.js
@@ -0,0 +1,107 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function withBlue(Comp) {
+  return class Blue extends React.Component {
+    render() {
+      return (
+        <div style={{ color: "blue" }}>
+          <Comp {...this.props} />
+        </div>
+      );
+    }
+  };
+}
+function withRandom(Comp) {
+  return class Random extends React.Component {
+    constructor(...args) {
+      super(...args);
+      this.state = {
+        randomNumber: undefined,
+        isLoading: false
+      };
+      this.handleClick = this.handleClick.bind(this);
+      this.handleDocumentScroll = this.handleDocumentScroll.bind(this);
+    }
+    handleClick() {
+      const { isLoading } = this.state;
+      if (isLoading) return;
+      this.fetchRandom();
+    }
+    handleDocumentScroll() {
+      //do somethings
+    }
+    fetchRandom() {
+      this.setState({
+        isLoading: true
+      });
+      setTimeout(() => {
+        this.setState({
+          randomNumber: (Math.random() * 100) | 0,
+          isLoading: false
+        });
+      }, 2000);
+    }
+    render() {
+      const { randomNumber, isLoading } = this.state;
+
+      return (
+        <div>
+          <p>class component</p>
+          <Comp {...this.props} randomNumber={randomNumber} />
+          <button disable={isLoading} onClick={this.handleClick}>
+            {isLoading ? "loading..." : "refresh"}
+          </button>
+        </div>
+      );
+    }
+
+    componentDidMount() {
+      document.title = `randomNumber  is ${this.state.randomNumber}`;
+      document.addEventListener("scroll", this.handleDocumentScroll);
+    }
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.randomNumber !== this.state.randomNumber) {
+        document.title = `randomNumber  is ${this.state.randomNumber}`;
+      }
+    }
+    componentWillUnmount() {
+      document.removeEventListener("scroll", this.handleDocumentScroll);
+    }
+  };
+}
+
+class ShowingRandom extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        HOC{this.props.showStyle}：randomNumber{this.props.randomNumber}
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {};
+  }
+  render() {
+    const WithRandom1 = withRandom(ShowingRandom);
+    const WithRandom2 = withBlue(withRandom(ShowingRandom)); //compose(withBlue,withRandom)(ShowingRandom)
+    return (
+      <>
+        <WithRandom1 showStyle="展现方式1" />
+        <WithRandom2 showStyle="展现方式2" />
+      </>
+    );
+  }
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
---
+++ src/render-props.js
@@ -0,0 +1,86 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class WithRandom extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      randomNumber: undefined,
+      isLoading: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleDocumentScroll = this.handleDocumentScroll.bind(this);
+  }
+  handleClick() {
+    const { isLoading } = this.state;
+    if (isLoading) return;
+    this.fetchRandom();
+  }
+  handleDocumentScroll() {
+    //do somethings
+  }
+  fetchRandom() {
+    this.setState({
+      isLoading: true
+    });
+    setTimeout(() => {
+      this.setState({
+        randomNumber: (Math.random() * 100) | 0,
+        isLoading: false
+      });
+    }, 2000);
+  }
+  render() {
+    const { randomNumber, isLoading } = this.state;
+
+    return (
+      <div>
+        <p>class component</p>
+        {this.props.children(randomNumber)}
+        <button disable={isLoading} onClick={this.handleClick}>
+          {isLoading ? "loading..." : "refresh"}
+        </button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    document.title = `randomNumber  is ${this.state.randomNumber}`;
+    document.addEventListener("scroll", this.handleDocumentScroll);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.randomNumber !== this.state.randomNumber) {
+      document.title = `randomNumber  is ${this.state.randomNumber}`;
+    }
+  }
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.handleDocumentScroll);
+  }
+}
+
+class App extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <WithRandom>
+          {number => {
+            return <p>显示方式1:{number}</p>;
+          }}
+        </WithRandom>
+        <WithRandom>
+          {number => {
+            return <p>显示方式2:{number}</p>;
+          }}
+        </WithRandom>
+      </div>
+    );
+  }
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
